08ead2e19c849771c76e521702a8ee3c9266c75b
unknown <worldrulesdev@gmail.com>
2020-05-18 03:06:48 -0300

    api create

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../api/youtube";
 
 export default class App extends Component {
+  onTermSubmit = (term) => {
+    const KEY = "AIzaSyBC5QBpNk711Ign99PSO4HFR00HyVLBJxU";
+
+    youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        type: "video",
+        key: KEY,
+      },
+    });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,6 +8,8 @@ export default class SearchBar extends Component {
   };
   onFormSubmit = (event) => {
     event.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
   };
 
   render() {
