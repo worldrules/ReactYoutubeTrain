b61194c1f20ca0e34da5119c463290a111f4f1e3
unknown <worldrulesdev@gmail.com>
2020-05-18 01:46:35 -0300

    Start components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  state = { term: "" };
+
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-export default class index extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+ReactDOM.render(<App />, document.querySelector("#root"));
