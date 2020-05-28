a4081747e28bf190a9b41f98cd8a5bb709e4a2b7
unknown <worldrulesdev@gmail.com>
2020-05-28 02:12:44 -0300

    Video item and childs

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
+import VideoList from "./VideoList";
 
 export default class App extends Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const KEY = "AIzaSyBC5QBpNk711Ign99PSO4HFR00HyVLBJxU";
@@ -22,11 +23,18 @@ export default class App extends Component {
     this.setState({ videos: datavideos });
   };
 
+  onVideoSelect = (video) => {
+    console.log("from the app", video);
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length}
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+import VideoItem from "./VideoItem";
 
-export default class VideoList extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
+  });
+
+  return <div className="ui relaxed divided list">{renderedList}</div>;
+};
+
+export default VideoList;
