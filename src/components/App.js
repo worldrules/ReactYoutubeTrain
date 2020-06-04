0d928b35b92109cd4bc93c06aa796dff54d6c37e
unknown <worldrulesdev@gmail.com>
2020-06-04 17:38:04 -0300

    I kiss the girl....

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 export default class App extends Component {
   state = { videos: [], selectedVideo: null };
@@ -24,13 +25,14 @@ export default class App extends Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("from the app", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
